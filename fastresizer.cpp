df11143e9c9258251c32e9d0848e897874c22f7d
Joshua French <josh.23.french@gmail.com>
2013-04-14 23:52:19 -0400

    New FastResizer class ... for multithreading image scaling.

---
+++ fastresizer.cpp
@@ -0,0 +1,6 @@
+#include "fastresizer.h"
+
+FastResizer::FastResizer(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ fastresizer.h
@@ -0,0 +1,18 @@
+#ifndef FASTRESIZER_H
+#define FASTRESIZER_H
+
+#include <QObject>
+
+class FastResizer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FastResizer(QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // FASTRESIZER_H
