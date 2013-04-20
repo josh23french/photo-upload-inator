265fa8d663c53a028c0df482b0b2bb08767558a1
Joshua French <josh.23.french@gmail.com>
2013-04-20 00:02:26 -0400

    Added global QSettings config

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QCoreApplication::setApplicationName("Directory Uploader");
+    QCoreApplication::setOrganizationDomain("lbcde.org")
+    QCoreApplication::setOrganizationName("LBC");
+
     TetherWindow tw;
     tw.show();
     app.connect(&tw, SIGNAL(imageSaved( const char * )), &tw, SLOT(displayThumbForTethered( const char * )));
